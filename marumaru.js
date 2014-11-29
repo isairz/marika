9cdb4ab04ad5b18fb6e240a7ec01eb6e8a5b96d8
Seongjun Kim <isair.zun@gmail.com>
2014-11-29 15:56:15 +0900

    Access protected contents.

--- marumaru.js
+++ marumaru.js
@@ -2,6 +2,16 @@
 var cheerio = require('cheerio');
 var request = require('request');
 
+var config;
+
+try {
+  config = require('./config.json');
+} catch (e) {
+  console.log(e);
+  console.warn("config isn't exist.\nRunning as default settings");
+  config = require('./config.sample.json');
+}
+
 var marumaru = module.exports = {};
 
 var req = function () {
@@ -12,16 +22,29 @@ var req = function () {
       'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.65 Safari/537.36'
     }
   });
+  var requestLogin = request.defaults({
+    jar: jar,
+    headers: {
+      'Referer': 'http://www.mangaumaru.com/archives/114053',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.65 Safari/537.36',
+      'Origin': 'http://www.mangaumaru.com'
+    }
+  });
+  var loginTryed = false;
 
   var _req = function (url, callback) {
     function tryRequest(url, callback) {
       requestBase(url, function(err, res, body) {
-
         if (body.indexOf('document.cookie=\'sucuri_uidc=') != -1) {
           // sucuri ddos protector
           var cookieRegex = /document\.cookie\.indexOf\('(sucuri_uidc=\w+)'\)/.exec(body);
           jar.setCookie(cookieRegex[1], url);
           tryRequest(url, callback);
+        } else if (body.indexOf('http://www.mangaumaru.com/wp-login.php?action=postpass') != -1) {
+          // login for protected manga.
+          _req.login(function (){
+            tryRequest(url, callback);
+          })
         } else {
           callback(err, res, body);
         }
@@ -29,6 +52,22 @@ var req = function () {
     }
 
     tryRequest(url, callback);
+  }
+
+  _req.login = function(callback) {
+    if (loginTryed) {
+      // Because of sucuri brute-force protector, retrying could cause ip ban. (30 times for 1 hour)
+      var e = new Error('Login Failed!');
+      callback(e);
+      return;
+    }
+
+    loginTryed = true;
+    requestLogin.post(
+      'http://www.mangaumaru.com/wp-login.php?action=postpass',
+      {form: {post_password: config.wp_password, Submit: 'Submit'}},
+      callback
+    );
   }
 
   return _req;
